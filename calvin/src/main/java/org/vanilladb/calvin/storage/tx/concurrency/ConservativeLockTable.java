16cd05e9e4a51de7d3cbe6b3c6a793779e41dcc3
105062206Tsai-Che-Wei <sponge611@gmail.com>
2020-06-26 16:10:00 +0800

    Plus anchor in LockTable

--- calvin/src/main/java/org/vanilladb/calvin/storage/tx/concurrency/ConservativeLockTable.java
+++ calvin/src/main/java/org/vanilladb/calvin/storage/tx/concurrency/ConservativeLockTable.java
@@ -5,7 +5,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Queue;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentLinkedQueue;
 
 import org.vanilladb.core.storage.tx.concurrency.LockAbortException;
 
@@ -26,7 +25,7 @@ public class ConservativeLockTable {
 		Lockers() {
 			sLockers = new LinkedList<Long>();
 			xLocker = NONE;
-			requestQueue = new ConcurrentLinkedQueue<Long>();
+			requestQueue = new LinkedList<Long>();
 		}
 		
 		@Override
@@ -39,12 +38,35 @@ public class ConservativeLockTable {
 	}
 
 	private Map<Object, Lockers> lockerMap = new ConcurrentHashMap<Object, Lockers>();
-
+	private final Object[] anchors = new Object[1009];
+	
+	/*
+	 * Get the anchor  
+	 *
+	 */
+	Object getAnchor(Object obj) {
+		int code = obj.hashCode() % anchors.length;
+		if (code < 0) {
+			code += anchors.length;
+		}
+		//For Debug
+		/*if(anchors[code] == null) {
+			System.out.println("Ooops");
+		}*/
+		return anchors[code];
+	}
+	
+	
+	
+	 
 	/**
 	 * Create and initialize a conservative ordered lock table.
 	 */
 	public ConservativeLockTable() {
-		
+		for (int i = 0; i < anchors.length; ++i) {
+			anchors[i] = new Object();
+		}
+
 	}
 
 	/**
@@ -57,8 +79,11 @@ public class ConservativeLockTable {
 	 *            the transaction that requests the lock
 	 */
 	void requestLock(Object obj, long txNum) {
-		Lockers lockers = prepareLockers(obj);
-		lockers.requestQueue.add(txNum);
+		Object anchor = getAnchor(obj);
+		synchronized(anchor) {
+			Lockers lockers = prepareLockers(obj);
+			lockers.requestQueue.add(txNum);
+		}
 	}
 
 	/**
@@ -73,61 +98,65 @@ public class ConservativeLockTable {
 	 *            a transaction number
 	 * 
 	 */
-	synchronized void sLock(Object obj, long txNum) {
-		Lockers lockers = prepareLockers(obj);
-
-		// check if it have already held the lock
-		if (hasSLock(lockers, txNum)) {
-			lockers.requestQueue.remove(txNum);
-			return;
-		}
-
-		try {
-			String name = Thread.currentThread().getName();
-			
-			/*
-			 * If this transaction is not the first one requesting this
-			 * object or it cannot get lock on this object, it must wait.
-			 */
-			Long head = lockers.requestQueue.peek();
-			while (!sLockable(lockers, txNum) || (head != null && head.longValue() != txNum)) {
-
-				// For debug
-				/*if (lockers.xLocker != -1) {
-					Thread.currentThread().setName(String.format(
-							"%s waits for slock of %s from tx.%d (xlock holder)",
-							name, obj, lockers.xLocker));
-				} else {
-					Thread.currentThread().setName(String.format(
-							"%s waits for slock of %s from tx.%d (head of queue)",
-							name, obj, head));
-				}*/
-				
-				wait();
-
-				// Since a transaction may delete the lockers of an object
-				// after releasing them, it should call prepareLockers()
-				// here, instead of using lockers it obtains earlier.
-				lockers = prepareLockers(obj);
-				head = lockers.requestQueue.peek();
+	void sLock(Object obj, long txNum) {
+		
+		Object anchor = getAnchor(obj);
+		synchronized(anchor) {
+			Lockers lockers = prepareLockers(obj);
+			// check if it have already held the lock
+			if (hasSLock(lockers, txNum)) {
+				lockers.requestQueue.remove(txNum);
+				return;
 			}
 
-			Thread.currentThread().setName(name);
+			try {
+				String name = Thread.currentThread().getName();
 			
-			if (!sLockable(lockers, txNum))
-				throw new LockAbortException();
+				/*
+				 * If this transaction is not the first one requesting this
+				 * object or it cannot get lock on this object, it must wait.
+				 */
+				Long head = lockers.requestQueue.peek();
+				while (!sLockable(lockers, txNum) || (head != null && head.longValue() != txNum)) {
 
-			// get the s lock
-			lockers.requestQueue.poll();
-			lockers.sLockers.add(txNum);
+					// For debug
+					/*if (lockers.xLocker != -1) {
+						Thread.currentThread().setName(String.format(
+								"%s waits for slock of %s from tx.%d (xlock holder)",
+								name, obj, lockers.xLocker));
+					} else {
+						Thread.currentThread().setName(String.format(
+								"%s waits for slock of %s from tx.%d (head of queue)",
+								name, obj, head));
+					}*/
+				
+					anchor.wait();
 
-			// Wake up other waiting transactions (on this object) to let
-			// them
-			// fight for the lockers on this object.
-			notifyAll();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-			throw new LockAbortException("Interrupted when waitting for lock");
+					// Since a transaction may delete the lockers of an object
+					// after releasing them, it should call prepareLockers()
+					// here, instead of using lockers it obtains earlier.
+					lockers = prepareLockers(obj);
+					head = lockers.requestQueue.peek();
+				}
+
+				Thread.currentThread().setName(name);
+			
+				if (!sLockable(lockers, txNum))
+					throw new LockAbortException();
+
+				// get the s lock
+				lockers.requestQueue.poll();
+				lockers.sLockers.add(txNum);
+
+				// Wake up other waiting transactions (on this object) to let
+				// them
+				// fight for the lockers on this object.
+				anchor.notifyAll();
+			
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+				throw new LockAbortException("Interrupted when waitting for lock");
+			}
 		}
 	}
 
@@ -143,54 +172,57 @@ public class ConservativeLockTable {
 	 *            a transaction number
 	 * 
 	 */
-	synchronized void xLock(Object obj, long txNum) {
-		Lockers lockers = prepareLockers(obj);
-
-		if (hasXLock(lockers, txNum)) {
-			lockers.requestQueue.remove(txNum);
-			return;
-		}
-
-		try {
-			String name = Thread.currentThread().getName();
-			
-			// long timestamp = System.currentTimeMillis();
-			Long head = lockers.requestQueue.peek();
-			while ((!xLockable(lockers, txNum) || (head != null && head.longValue() != txNum))
-			/* && !waitingTooLong(timestamp) */) {
-				
-				// For debug
-				/*if (lockers.xLocker != -1) {
-					Thread.currentThread().setName(String.format(
-							"%s waits for xlock of %s from tx.%d (xlock holder)",
-							name, obj, lockers.xLocker));
-				} else if (!lockers.sLockers.isEmpty()) {
-					Thread.currentThread().setName(String.format(
-							"%s waits for xlock of %s from tx.%d (slock holder, %d other holders)",
-							name, obj, lockers.sLockers.get(0), lockers.sLockers.size() - 1));
-				} else {
-					Thread.currentThread().setName(String.format(
-							"%s waits for xlock of %s from tx.%d (head of queue)",
-							name, obj, head));
-				}*/
-				
-				wait();
-				lockers = prepareLockers(obj);
-				head = lockers.requestQueue.peek();
+	void xLock(Object obj, long txNum) {
+		Object anchor = getAnchor(obj);
+		synchronized(anchor) {
+			Lockers lockers = prepareLockers(obj);
+			if (hasXLock(lockers, txNum)) {
+				lockers.requestQueue.remove(txNum);
+				return;
 			}
 
-			Thread.currentThread().setName(name);
+			try {
+				String name = Thread.currentThread().getName();
 			
-			// if (!xLockable(lockers, txNum))
-			// throw new LockAbortException();
-			// get the x lock
-			lockers.requestQueue.poll();
-			lockers.xLocker = txNum;
+				// long timestamp = System.currentTimeMillis();
+				Long head = lockers.requestQueue.peek();
+				while ((!xLockable(lockers, txNum) || (head != null && head.longValue() != txNum))
+						/* && !waitingTooLong(timestamp) */) {
+				
+					// For debug
+					/*if (lockers.xLocker != -1) {
+						Thread.currentThread().setName(String.format(
+							"%s waits for xlock of %s from tx.%d (xlock holder)",
+							name, obj, lockers.xLocker));
+					} else if (!lockers.sLockers.isEmpty()) {
+						Thread.currentThread().setName(String.format(
+							"%s waits for xlock of %s from tx.%d (slock holder, %d other holders)",
+							name, obj, lockers.sLockers.get(0), lockers.sLockers.size() - 1));
+					} else {
+						Thread.currentThread().setName(String.format(
+							"%s waits for xlock of %s from tx.%d (head of queue)",
+							name, obj, head));
+					}*/
+				
+					anchor.wait();
+					lockers = prepareLockers(obj);
+					head = lockers.requestQueue.peek();
+				}
 
-			// An X lock blocks all other lockers, so it don't need to
-			// wake up anyone.
-		} catch (InterruptedException e) {
-			throw new LockAbortException("Interrupted when waitting for lock");
+				Thread.currentThread().setName(name);
+			
+				// if (!xLockable(lockers, txNum))
+				// throw new LockAbortException();
+				// get the x lock
+				lockers.requestQueue.poll();
+				lockers.xLocker = txNum;
+
+				// An X lock blocks all other lockers, so it don't need to
+				// wake up anyone.
+			
+			} catch (InterruptedException e) {
+				throw new LockAbortException("Interrupted when waitting for lock");
+			}
 		}
 	}
 
@@ -206,22 +238,24 @@ public class ConservativeLockTable {
 	 * @param lockType
 	 *            the type of lock
 	 */
-	synchronized void release(Object obj, long txNum, LockType lockType) {
-		Lockers lks = lockerMap.get(obj);
+	void release(Object obj, long txNum, LockType lockType) {
 		
-		if (lks == null)
-			return;
+		Object anchor = getAnchor(obj);
+		synchronized(anchor) {
+			Lockers lks = lockerMap.get(obj);
+			if (lks == null)
+				return;
 		
-		releaseLock(lks, txNum, lockType);
-
-		// Remove the locker, if there is no other transaction
-		// holding it
-		if (!sLocked(lks) && !xLocked(lks)
-				&& lks.requestQueue.isEmpty())
-			lockerMap.remove(obj);
+			releaseLock(lks, txNum, lockType);
+			// Remove the locker, if there is no other transaction
+			// holding it
+			if (!sLocked(lks) && !xLocked(lks)
+					&& lks.requestQueue.isEmpty())
+				lockerMap.remove(obj);
 		
-		// There might be someone waiting for the lock
-		notifyAll();
+			// There might be someone waiting for the lock
+			anchor.notifyAll();
+		}
 	}
 
 	private Lockers prepareLockers(Object obj) {
@@ -234,7 +268,7 @@ public class ConservativeLockTable {
 	}
 
 	private void releaseLock(Lockers lks, long txNum, LockType lockType) {
-		notifyAll();
+		//notifyAll();
 		switch (lockType) {
 		case X_LOCK:
 			if (lks.xLocker == txNum) {
